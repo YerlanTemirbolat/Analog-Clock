d1ac27aa2b94191f086cb2625fd9746e48c38db5
YerlanTemirbolat <erlantemirbolat87@gmail.com>
2020-09-09 21:49:00 +0600

    Create Clock App

---
+++ Clock App/Clock_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clock_AppApp.swift
+//  Clock App
+//
+//  Created by Admin on 9/9/20.
+//
+
+import SwiftUI
+
+@main
+struct Clock_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
